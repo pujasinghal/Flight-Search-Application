617e2e75a5c3d28e6e4fe0fb7806260b5e7d3e14
Chuckles <poosinghal123@gmail.com>
2017-09-11 19:24:02 +0530

    Complete Application

--- scripts/controller/flightApp.js
+++ scripts/controller/flightApp.js
@@ -4,6 +4,13 @@ app.controller('FlightCtrl',['$scope','GetJsonDataFact','$filter',function ($sco
 	var newArr = [];
 	$scope.cities = null;
 	$scope.returnShow = true;
+	$scope.min = 0;
+	$scope.max = 10000;
+	$scope.oneWayData = [];
+	$scope.allFilter1 = [];
+	$scope.allFilter2 = [];
+	$scope.twoWayData = [];
+	$scope.showResults = false;
 
 	GetJsonDataFact.getData().then(function(resp) {
 		$scope.allAirports = resp.data.airports;
@@ -16,9 +23,6 @@ app.controller('FlightCtrl',['$scope','GetJsonDataFact','$filter',function ($sco
 			console.log(resp);
 	});
 	
-	// $scope.getDate = function() {
-	// 	console.log($filter('date')($scope.depDate.value,"dd-MM-yyyy"));
-	// }
 	$scope.autoComplete = function(fromCity,toCity) {
 		if(fromCity) {
 		    $scope.cities = newArr.filter(function(e) {
@@ -30,7 +34,6 @@ app.controller('FlightCtrl',['$scope','GetJsonDataFact','$filter',function ($sco
 		    	if($scope.searchToCity !== undefined ) return (e.indexOf($scope.searchToCity.toUpperCase()) !== -1 && e !== $scope.searchFromCity);
 		    });				
 		}
-
   	};
 
 	$scope.slider = {
@@ -40,65 +43,100 @@ app.controller('FlightCtrl',['$scope','GetJsonDataFact','$filter',function ($sco
 	    floor: 0,
 	    ceil: 10000,
 	    step: 1000,
-    	showTicks: true
+    	showTicks: true,
+        onEnd: function() {
+            console.log('Apply filters');
+            $scope.applyPriceFilter();
+        }    	
 	  }
 	};
+	
+	$scope.mindate = new Date();
 	
 	$scope.returnDate = function(input) {
 		$scope.returnShow = input;
 	};
 
-	$scope.searchFlight = function() {
-		angular.forEach($scope.allAirports,function(key,value) {
-			if(key.label.indexOf($scope.searchFromCity) > -1) {
-				$scope.fromAirportId = key.airportId;	
-				$scope.fromAirportValue = key.value;
-			}
-			if(key.label.indexOf($scope.searchToCity) > -1) {
-				$scope.toAirportId = key.airportId;	
-				$scope.toAirportValue = key.value;			
-			}		
+	$scope.searchFlight = function() {		
+		$scope.convertDate = function(input) {
+			return $filter('date')(input,"MM-dd-yyyy");
+		};
+
+		$scope.formatDate = function(input) {
+			var newDate = new Date(input);
+			return $filter('date')(newDate,'shortTime');
+		};
+
+		$scope.selFromAirport = $filter('filter')($scope.allAirports,{'label': $scope.searchFromCity});
+		$scope.selToAirport = $filter('filter')($scope.allAirports,{'label': $scope.searchToCity});
+
+		$scope.selFromCity = $filter('filter')($scope.allCities,{'airportId': $scope.selFromAirport[0].airportId});
+		$scope.selToCity = $filter('filter')($scope.allCities,{'airportId': $scope.selToAirport[0].airportId});
+
+		GetJsonDataFact.getFlightData()
+		.then(function(resp) {
+			$scope.flights = resp.data;
+			$scope.filterData();
+			$scope.showResults = true;
+		}, function(resp) {
+			$scope.showResults = false;
+			console.log(resp);
 		});
 
-		angular.forEach($scope.allCities,function(key,value) {
-			if(key.airportId.indexOf($scope.fromAirportId) > -1) {
-				$scope.fromCityId = key.cityId;
+		$scope.filterData = function() {
+			$scope.allFilter1 = $scope.flights.filter(function(obj) {
+				if(obj.depCityId == $scope.selFromCity[0].cityId  
+					&& obj.arrCityId == $scope.selToCity[0].cityId 
+					&& obj.depDateTime.indexOf($scope.convertDate($scope.depDate.value)) > -1) {
+
+					obj.depCityId = $scope.selFromAirport[0].value
+					obj.arrCityId = $scope.selToAirport[0].value;
+					obj.depDateTime = $scope.formatDate(obj.depDateTime);					
+					obj.arrDateTime = $scope.formatDate(obj.arrDateTime);
+					return obj;
+				}
+			});
+			if(!$scope.returnShow) {
+				$scope.allFilter2 = $scope.flights.filter(function(obj) {
+					if(obj.depCityId == $scope.selToCity[0].cityId 
+						&& obj.arrCityId == $scope.selFromCity[0].cityId 
+						&& obj.depDateTime.indexOf($scope.convertDate($scope.arrDate.value)) > -1) {
+
+						obj.depCityId = $scope.selToAirport[0].value
+						obj.arrCityId = $scope.selFromAirport[0].value;
+						obj.depDateTime = $scope.formatDate(obj.depDateTime);	
+						obj.arrDateTime = $scope.formatDate(obj.arrDateTime);
+						return obj;
+					}
+				});		
 			}
-			if(key.airportId.indexOf($scope.toAirportId) > -1) {
-				$scope.toCityId = key.cityId;		
-			}		
-		});		
-		$scope.convertDate = function(date) {
-			return $filter('date')(date,"dd-MM-yyyy")
+			$scope.oneWayData = $scope.allFilter1;
+			$scope.twoWayData = $scope.allFilter2;
+			$scope.applyPriceFilter();
 		}
-		var searchFilter = null;
-		/* OneWay Search*/
-		if(returnShow) {
-			searchFilter = {
-				depCityId:$scope.fromCityId,
-				arrCityId:$scope.toCityId,
-				depDateTime:$scope.convertDate($scope.depDate.value)
-			};
-		} else {
-			searchFilter = {
-				depCityId: $scope.fromCityId || $scope.toCityId ,
-				arrCityId :$scope.toCityId || $scope.fromCityId,
-				depDateTime: $scope.convertDate($scope.depDate.value),
-				arrDateTime: $scope.convertDate($scope.arrDate.value),
-			};			
-
-		}		
-		GetJsonDataFact.getFlightData(searchFilter)
-		.then(function(resp) {
-			$scope.allFlights = resp.data[0];
-			//var arr = $scope.allAirports;
-			
-			// angular.forEach(arr,function(key,value) {
-			// 	newArr.push(key.label.toUpperCase());
-			// });		
-		}, function(resp) {
-			console.log(resp);
-		});						
-
 	};
+
+	$scope.applyPriceFilter = function() {
+		if($scope.allFilter1.length !== 0 ) {
+			$scope.oneWayData = $scope.allFilter1.filter(function(obj) {
+				if(obj.price >= $scope.slider.min && obj.price <= $scope.slider.max) {
+					return obj;
+				}
+			});			
+		}
+		if(!$scope.returnShow && $scope.allFilter2.length !== 0) {
+			$scope.twoWayData = $scope.allFilter2.filter(function(obj) {
+				if(obj.price >= $scope.slider.min && obj.price <= $scope.slider.max) {
+					return obj;
+				}
+			});
+		}
+		if(($scope.oneWayData.length === 0 && $scope.returnShow) 
+			|| ($scope.oneWayData.length === 0 && $scope.twoWayData.length === 0)) {
+			$scope.noData = true;
+		} else {
+			$scope.noData = false;
+		}
+	};	
+
 }]);
--- scripts/services/fetchService.js
+++ scripts/services/fetchService.js
@@ -14,26 +14,12 @@ angular.module('fetchService',[])
 			deferred.reject(message);
 		});
 		return deferred.promise;
-	};
-	factObj.getOperators= function() {
+	};	
+	factObj.getFlightData= function() {
 		var deferred = $q.defer();
 		$http({
 			method:'GET',
 			url:'json/flightData.json'
-		}).then(function success(resp) {
-			deferred.resolve(resp);
-		}, function error(message) {
-			console.log(message);
-			deferred.reject(message);
-		});
-		return deferred.promise;
-	};	
-	factObj.getFlightData= function(searchFilters) {
-		var deferred = $q.defer();
-		$http({
-			method:'GET',
-			url:'json/data.json',
-			params: searchFilters
 		}).then(function success(resp) {
 			deferred.resolve(resp);
 		}, function error(message) {
